a48a0e47d9e58da78f69235284f1fe453ee5681b
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-07-02 11:11:00 +0200

    Add a memory example with chat

--- tests/contrib/llms/test_open_ai.py
+++ tests/contrib/llms/test_open_ai.py
@@ -3,6 +3,7 @@ from typing import (
     Any,
     AsyncGenerator,
     List,
+    TypedDict,
 )
 
 import pytest
@@ -90,3 +91,56 @@ class OpenAIChatChainTestCase(unittest.IsolatedAsyncioTestCase):
             print(output.output["content"], end="", flush=True)
             result += output.output["content"]
         self.assertIn("Hello Alice! How can I assist you today?", result)
+
+    @pytest.mark.integration
+    async def test_it_simulates_memory(self):
+        class Memory(TypedDict):
+            history: List[OpenAIChatMessage]
+
+        memory = Memory(history=[])
+
+        def save_message_to_memory(message: OpenAIChatMessage) -> OpenAIChatMessage:
+            memory["history"].append(message)
+            return message
+
+        def update_delta_on_memory(delta: OpenAIChatDelta) -> OpenAIChatDelta:
+            if (
+                memory["history"][-1]["role"] != delta["role"]
+                and delta["role"] is not None
+            ):
+                memory["history"].append(
+                    OpenAIChatMessage(role=delta["role"], content=delta["content"])
+                )
+            else:
+                memory["history"][-1]["content"] += delta["content"]
+            return delta
+
+        chain = OpenAIChatChain[str, OpenAIChatDelta](
+            "EmojiChatChain",
+            lambda user_message: [
+                *memory["history"],
+                save_message_to_memory(
+                    OpenAIChatMessage(
+                        role="user", content=f"{user_message}. Reply in emojis"
+                    )
+                ),
+            ],
+            model="gpt-3.5-turbo-0613",
+            temperature=0,
+        ).map(update_delta_on_memory)
+
+        result = ""
+        async for output in chain("Hey there, my name is 🧨 how is it going?"):
+            if output.final:
+                print(output.output["content"], end="", flush=True)
+                result += output.output["content"]
+        self.assertIn("👋🧨", result)
+
+        result = ""
+        async for output in chain("What is my name?"):
+            if output.final:
+                print(output.output["content"], end="", flush=True)
+                result += output.output["content"]
+        self.assertIn("🧨", result)
+
+        self.assertEqual(len(memory["history"]), 4)
