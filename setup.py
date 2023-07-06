8afe81f5c2b45bdd11a7937b549b65c584f7e74c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-07-06 18:28:13 +0200

    Make setup.py more complete

--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
 from setuptools import setup, find_packages
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
-    name='litechain',
-    version='0.1',
+    name="litechain",
+    version="0.1",
     packages=find_packages(),
     install_requires=requirements,
-    author='Rogerio Chaves',
-    author_email='rogeriocfj@gmail.com',
-    description='Build robust LLM applications with true composability 🔗',
-    license='MIT',
-    keywords='chain llm ai stream functional programming',
-    url='https://github.com/rogeriochaves/litechain',
+    author="Rogerio Chaves",
+    author_email="rogeriocfj@gmail.com",
+    description="Build robust LLM applications with true composability 🔗",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    keywords="chain llm ai stream functional programming",
+    url="https://github.com/rogeriochaves/litechain",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    python_requires=">=3.6",
+    project_urls={
+        "Documentation": "https://rogeriochaves.github.io/litechain/",
+        "Source Code": "https://github.com/rogeriochaves/litechain",
+        "Issue Tracker": "https://github.com/rogeriochaves/litechain/issues",
+    },
+    include_package_data=True,
 )
