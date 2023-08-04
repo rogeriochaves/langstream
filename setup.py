db9451746ac240381d0b29d986eb0bd412d98d3c
Rogério Chaves <rogeriochaves@users.noreply.github.com>
2023-08-04 07:42:07 +0200

    Bump to v0.1.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="langstream",
-    version="0.1.6",
+    version="0.1.7",
     packages=find_packages(),
     install_requires=requirements,
     author="Rogerio Chaves",
